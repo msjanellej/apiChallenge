1d453662c27df1d12d679d9c509f6e24faa1209d
Janelle Johnson <janelleblevins@gmail.com>
2020-12-17 10:31:29 -0600

    added review class

---
+++ reviewapi/reviewapi/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace reviewapi
+{
+     public class Review
+    {
+        public string reviewerName;
+        public string avatarUrl;
+        public string rating;
+        public string reviewContent;
+
+
+        public Review()
+        {
+
+
+        }
+    }
+}
